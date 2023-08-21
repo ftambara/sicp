06319d0f362486866d4e2a08aec4403eb1ca4dba
ftambara <98750668+ftambara@users.noreply.github.com>
2023-08-21 20:59:11 -0300

    add exercise 2.2

--- chapter2/section2-1.rkt
+++ chapter2/section2-1.rkt
@@ -70,3 +70,32 @@
                     (- (abs divisor))
                     (abs divisor))))
          (cons (/ num norm-div) (/ den norm-div))))
+
+; Exercise 2.2
+(define (print-point p)
+    (newline)
+    (display "(")
+    (display (x-point p))
+    (display ",")
+    (display (y-point p))
+    (display ")"))
+
+(define (average a b) (/ (+ a b) 2))
+
+(define (midpoint-segment segment)
+    (make-point (average (x-point (start-segment segment))
+                         (x-point (end-segment segment)))
+                (average (y-point (start-segment segment))
+                         (y-point (end-segment segment)))))
+
+(define (make-point x y) (cons x y))
+
+(define (x-point point) (car point))
+
+(define (y-point point) (cdr point))
+
+(define (make-segment start end) (cons start end))
+
+(define (start-segment segment) (car segment))
+
+(define (end-segment segment) (cdr segment))
