f06230634135f467f7f490968545fcd78f910c22
ftambara <98750668+ftambara@users.noreply.github.com>
2023-08-26 14:20:17 -0300

    add reverse-list-app implementation and comparison

--- chapter2/exercises/2.18.rkt
+++ chapter2/exercises/2.18.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(provide reverse-list)
+(provide reverse-list reverse-list-app time-procedure)
+
+(require "../utils.rkt")
 
 (define (reverse-list list_)
     (define (iter reversed remain)
@@ -8,3 +10,45 @@
             reversed
             (iter (cons (car remain) reversed) (cdr remain))))
     (iter '() list_))
+
+; reverse-list could also be defined with the help of
+; append, but that would make the procedure O(n^2) for time
+; this implementation is O(n) in both time and memory.
+(define (reverse-list-app list_)
+    (if (null? list_)
+        list_
+        (append (reverse-list-app (cdr list_)) (list (car list_)))))
+
+(define list4 (build-list 10000 values))
+(define list5 (build-list 100000 values))
+(define list6 (build-list 1000000 values))
+
+(display "reverse-list\n")
+(time-procedure (lambda () (reverse-list list4)) 10)
+(time-procedure (lambda () (reverse-list list5)) 10)
+(time-procedure (lambda () (reverse-list list6)) 10)
+
+(newline)
+(display "reverse-list-app\n")
+(time-procedure (lambda () (reverse-list-app list4)) 10)
+; (time-procedure (lambda () (reverse-list-app list5)) 1)
+;   Run only once, commented out, takes too long
+; (time-procedure (lambda () (reverse-list-app list6)) 10)
+;   Don't run, would take hours
+
+; Results:
+; reverse-list
+; Average: 0.3 ms
+; Standard deviation: 0.45... ms
+; Average: 4.2 ms
+; Standard deviation: 2.71... ms
+; Average: 48.7 ms
+; Standard deviation: 7.23... ms
+
+; reverse-list-app
+; Average: 223.9 ms
+; Standard deviation: 6.22... ms
+; Average: 44837.0 ms
+; Standard deviation: 0 ms
+
+; Both sets of measurments agree with what it was theorized.
---
+++ chapter2/utils.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(provide time-procedure)
+
+(define (time-procedure proc n)
+    ; Show average and standard deviation
+    ; of running proc for n number of repetitions
+    (define (average items)
+        (/ (apply + items) (length items)))
+    (define (square x) (* x x))
+    (define (stddev items)
+        (sqrt (average (map square (map - items (build-list (length items) (lambda (x) (average items))))))))
+    (define (time-1 proc)
+        (let ((start (current-milliseconds)))
+            (proc)
+            (- (current-milliseconds) start)))
+    (let ((times (map time-1 (build-list n (lambda (x) proc)))))
+        (printf "Average: ~a ms\n" (exact->inexact (average times)))
+        (printf "Standard deviation: ~a ms\n" (stddev times))))
