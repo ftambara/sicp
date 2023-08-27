eedc06ddedd42b1cebed2cda9cb203c9929aa1e0
ftambara <98750668+ftambara@users.noreply.github.com>
2023-08-27 08:33:00 -0300

    remove debugging calls on 2.20.rkt

--- chapter2/exercises/2.20.rkt
+++ chapter2/exercises/2.20.rkt
@@ -2,17 +2,13 @@
 
 (provide same-parity)
 
-(define (echo x)
-    (display x)
-    (newline)
-    x)
 (define (same-parity ref . integers)
     (define (include? num)
-        (or (echo (and (even? num) (even? ref)))
+        (or (and (even? num) (even? ref))
             (and (odd? num) (odd? ref))))
     (cond ((null? integers) ref)
           ((include? (car integers))
-            (cons (car integers) (same-parity ref (echo (cdr integers)))))
+            (cons (car integers) (same-parity ref (cdr integers))))
           (else (same-parity (cdr integers)))))
 
 (same-parity 1 2 3 4 5 6 7); => (1 3 5 7)
