d477f7bd8bd04b2b93faeaeee7f64779395c2877
ftambara <98750668+ftambara@users.noreply.github.com>
2023-08-29 07:29:10 -0300

    add exercise 2.28

---
+++ chapter2/exercises/2.28.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(require rackunit)
+
+
+; returns a list whose elements are all the leaves of the
+; tree arranged in left-to-right order
+(define (fringe tree)
+    (cond ((null? tree) tree)
+          ((list? tree)
+            (append (fringe (car tree)) (fringe (cdr tree))))
+          (else (list tree))))
+
+(define x (list (list 1 2) (list 3 4)))
+(check-equal? (fringe x) '(1 2 3 4))
+(check-equal? (fringe (list x x)) '(1 2 3 4 1 2 3 4))
