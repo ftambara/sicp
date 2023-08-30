b5c6d047580ef53b384e466e6ca0d45cf6f67bc2
ftambara <98750668+ftambara@users.noreply.github.com>
2023-08-29 21:55:53 -0300

    add exercise 2.31

---
+++ chapter2/exercises/2.31.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(require rackunit)
+
+
+(define (tree-map proc tree)
+    (map (lambda (node)
+            (if (pair? node)
+                (tree-map proc node)
+                (proc node)))
+         tree))
+
+(define (square x)
+    (* x x))
+
+(define (square-tree tree) (tree-map square tree))
+
+(check-equal? 
+    (square-tree
+        (list 1
+            (list 2 (list 3 4) 5)
+            (list 6 7)))
+    '(1 (4 (9 16) 25) (36 49)))
