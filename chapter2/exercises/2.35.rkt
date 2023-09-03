c31036f9fd509473ca5d39c86bc50a6d56e91304
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-03 09:47:11 -0300

    add exercise 2.35

---
+++ chapter2/exercises/2.35.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(require
+    "../utils.rkt"
+    rackunit)
+
+
+(define (count-leaves t)
+    (accumulate
+        (lambda (child-count accum) (+ child-count accum))
+        0
+        (map (lambda (node)
+                (cond ((null? node) 0)
+                      ((list? node) (count-leaves node))
+                      (else 1)))
+             t)))
+
+(check-equal? (count-leaves '(1 (2 3) (5 (6)))) 5)
