cf1d85ca1794ae2fd9a8b1ad62f8094561968677
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-03 07:51:01 -0300

    add conventional interfaces definitions

--- chapter2/section2-2.rkt
+++ chapter2/section2-2.rkt
@@ -50,3 +50,70 @@
                 (scale-tree sub-tree factor)
                 (* factor sub-tree)))
          tree))
+
+(define (filter predicate sequence)
+    (cond ((null? sequence) sequence)
+          ((predicate (car sequence))
+            (cons (car sequence) (filter predicate (cdr sequence))))
+          (else (filter predicate (cdr sequence)))))
+
+(filter odd? '(1 2 3 5 90 99))
+; => (1 3 5 99)
+
+; (define (accumulate op initial sequence)
+;     (if (null? sequence)
+;         initial
+;         (accumulate op (op (car sequence) initial) (cdr sequence))))
+; This implementation I thought of doesn't work for an op like cons,
+; since it reverses the intended result
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))
+
+
+(accumulate + 0 (list 1 2 3 4 5))
+; => 15
+(accumulate * 1 (list 1 2 3 4 5))
+; => 120
+(accumulate cons null (list 1 2 3 4 5))
+; => (1 2 3 4 5)
+
+(define (enumerate-interval low high)
+    (if (> low high)
+        null
+        (cons low (enumerate-interval (add1 low) high))))
+
+(enumerate-interval 2 7)
+; (2 3 4 5 6 7)
+
+(define (enumerate-tree tree)
+    (cond ((null? tree) tree)
+          ((list? tree)
+            (append (enumerate-tree (car tree))
+                    (enumerate-tree (cdr tree))))
+          (else (list tree))))
+
+(enumerate-tree (list 1 (list 2 (list 3 4)) 5))
+; (1 2 3 4 5)
+
+(define (square x)
+    (* x x))
+
+(define (fib n)
+    (define (iter two_ago one_ago count)
+        (if (= count n)
+            (+ two_ago one_ago)
+            (iter one_ago (+ two_ago one_ago) (+ count 1))))
+    (iter 1 0 0))
+
+(define (sum-odd-squares tree)
+    (accumulate + 0 (map square (filter odd? (enumerate-tree tree)))))
+
+(define (even-fibs n)
+    ; Why use an identity operation like this accumulate?
+    ; Is the consistency worth it?
+    (accumulate
+        cons '() (filter even? (map fib (enumerate-interval 0 n)))))
