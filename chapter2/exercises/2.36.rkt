867e9fe968c3a20e22030b55b3b83061bc2866ed
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-03 11:51:18 -0300

    add exercise 2.36

---
+++ chapter2/exercises/2.36.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(require
+    "../utils.rkt"
+    rackunit)
+
+(define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+        null
+        (cons (accumulate op init (map car seqs))
+              (accumulate-n op init (map cdr seqs)))))
+
+(check-equal?
+    (accumulate-n + 0 '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+    '(22 26 30))
