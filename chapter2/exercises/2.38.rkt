26bc574837900b675357b02ada165e7f14439399
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-04 07:32:36 -0300

    add exercise 2.38

---
+++ chapter2/exercises/2.38.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require rackunit)
+
+(define (fold-right op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (fold-right op initial (cdr sequence)))))
+
+
+(define (fold-left op initial sequence)
+    (define (iter result rest)
+        (if (null? rest)
+            result
+            (iter (op result (car rest))
+                  (cdr rest))))
+    (iter initial sequence))
+
+
+(check-equal? 3/2 (fold-right / 1 (list 1 2 3)))
+(check-equal? 1/6 (fold-left / 1 (list 1 2 3)))
+(check-equal? '(1 (2 (3 ()))) (fold-right list null (list 1 2 3)))
+(check-equal? '(((() 1) 2) 3) (fold-left list null (list 1 2 3)))
+
+(check-equal? 3/8 (fold-right / 1 (list 1 2 3 4)))
+(check-equal? 1/24 (fold-left / 1 (list 1 2 3 4)))
