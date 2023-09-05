2e5c056983b0402cce8cf4f03aee12e1b5c985d6
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-05 07:33:07 -0300

    reorder utils.rkt procedures alphabetically

--- chapter2/utils.rkt
+++ chapter2/utils.rkt
@@ -6,10 +6,32 @@
          time-procedure)
 
 
+; From the book
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))
+
 (define (echo x)
     (display x)
     (newline)
     x)
+
+(define (prime? number)
+    (define (expmod base exp m)
+        (cond ((= exp 0) 1)
+              ((even? exp)
+                (remainder (square (expmod base (/ exp 2) m)) m))
+              (else
+                (remainder (* base (expmod base (- exp 1) m)) m))))
+    (define (fermat-test n times)
+        (define (try-it a)
+            (= (expmod a n n) a))
+        (or (= times 0)
+            (and (try-it (+ 1 (random (- n 1))))
+                 (fermat-test n (- times 1)))))
+    (fermat-test number (max number 100)))
 
 (define (time-procedure proc n)
     ; Show average and standard deviation
@@ -26,25 +48,3 @@
     (let ((times (map time-1 (build-list n (lambda (x) proc)))))
         (printf "Average: ~a ms\n" (exact->inexact (average times)))
         (printf "Standard deviation: ~a ms\n" (stddev times))))
-
-; From the book
-(define (accumulate op initial sequence)
-    (if (null? sequence)
-        initial
-        (op (car sequence)
-            (accumulate op initial (cdr sequence)))))
-
-(define (prime? number)
-    (define (expmod base exp m)
-        (cond ((= exp 0) 1)
-              ((even? exp)
-                (remainder (square (expmod base (/ exp 2) m)) m))
-              (else
-                (remainder (* base (expmod base (- exp 1) m)) m))))
-    (define (fermat-test n times)
-        (define (try-it a)
-            (= (expmod a n n) a))
-        (or (= times 0)
-            (and (try-it (+ 1 (random (- n 1))))
-                 (fermat-test n (- times 1)))))
-    (fermat-test number (max number 100)))
