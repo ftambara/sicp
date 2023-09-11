2e54635ab72c877b4690674729d1ef288ee28a8e
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-11 08:26:57 -0300

    fix memq function from notes

--- chapter2/section2.3/notes.rkt
+++ chapter2/section2.3/notes.rkt
@@ -5,12 +5,12 @@
 
 (define (memq item sequence)
   (cond ((null? sequence) false)
-        ((eq? item (car sequence)) item)
+        ((eq? item (car sequence)) sequence)
         (else (memq item (cdr sequence)))))
 
 (check-false (memq 'apple '(banana pear orange)))
 (check-false (memq 'apple '(bananas pears apples)))
-(check-equal? (memq 'apple '(banana pear apple)) 'apple)
-(check-equal? (memq 'apple '(banana pear (apple peach))) '(apple peach))
-(memq 'apple '(x (apple sauce) y apple pear))
+(check-equal? (memq 'apple '(banana pear apple)) '(apple))
+(check-equal?
+ (memq 'apple '(banana (apple peach) apple pear)) '(apple pear))
 
