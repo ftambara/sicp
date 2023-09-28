1dc61724a7a875326e603ade7a23e3dff3706f11
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-27 21:23:55 -0300

    Add data-directed programming notes

--- chapter2/section2.4/notes.rkt
+++ chapter2/section2.4/notes.rkt
@@ -141,40 +141,128 @@
 
 ;; Generic operations
 
-(define (real-part z)
-  (cond ((rectangular? z)
-         (real-part-rectangular (contents z)))
-        ((polar? z)
-         (real-part-polar (contents z)))
-        (else (error "Unknown type: REAL-PART" z))))
+; (define (real-part z)
+;   (cond ((rectangular? z)
+;          (real-part-rectangular (contents z)))
+;         ((polar? z)
+;          (real-part-polar (contents z)))
+;         (else (error "Unknown type: REAL-PART" z))))
 
-(define (imag-part z)
-  (cond ((rectangular? z)
-         (imag-part-rectangular (contents z)))
-        ((polar? z)
-         (imag-part-polar (contents z)))
-        (else (error "Unknown type: IMAG-PART" z))))
+; (define (imag-part z)
+;   (cond ((rectangular? z)
+;          (imag-part-rectangular (contents z)))
+;         ((polar? z)
+;          (imag-part-polar (contents z)))
+;         (else (error "Unknown type: IMAG-PART" z))))
 
-(define (magnitude z)
-  (cond ((rectangular? z)
-         (magnitude-rectangular (contents z)))
-        ((polar? z)
-         (magnitude-polar (contents z)))
-        (else (error "Unknown type: MAGNITUDE" z))))
+; (define (magnitude z)
+;   (cond ((rectangular? z)
+;          (magnitude-rectangular (contents z)))
+;         ((polar? z)
+;          (magnitude-polar (contents z)))
+;         (else (error "Unknown type: MAGNITUDE" z))))
 
-(define (angle z)
-  (cond ((rectangular? z)
-         (angle-rectangular (contents z)))
-        ((polar? z)
-         (angle-polar (contents z)))
-        (else (error "Unknown type: ANGLE" z))))
+; (define (angle z)
+;   (cond ((rectangular? z)
+;          (angle-rectangular (contents z)))
+;         ((polar? z)
+;          (angle-polar (contents z)))
+;         (else (error "Unknown type: ANGLE" z))))
 
 (define (add-complex z1 z2)
   (make-from-real-imag (+ (real-part z1) (real-part z2))
                        (+ (imag-part z1) (imag-part z2))))
 
+; (define (make-from-real-imag x y)
+;   (make-from-real-imag-rectangular x y))
+
+; (define (make-from-mag-ang r a)
+;   (make-from-mag-ang-polar r a))
+
+
+;; Data-directed programming
+
+;; This approach solves two weaknesses of the previous
+;; implementation: manual type dispatch and name conflicts.
+;; Where does the 'data-directed' name mean in this context?
+
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (expt (real-part z) 2)
+             (expt (imag-part z) 2))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+
+  'done)
+
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z) (* (magnitude z) (cos (angle z))))
+  (define (imag-part z) (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (expt x 2) (expt y 2)))
+          (atan y x)))
+
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+
+  'done)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types: APPLY-GENERIC"
+                 (list op type-tags))))))
+
+;; Generic procedures using type dispatch
+(define (real-part z) (apply-generic 'real-part z))
+
+(define (imag-part z) (apply-generic 'imag-part z))
+
+(define (magnitude z) (apply-generic 'magnitude z))
+
+(define (angle z) (apply-generic 'angle z))
+
+;; Contrary to the above generic procedures, these
+;; constructors depend on available implementations
 (define (make-from-real-imag x y)
-  (make-from-real-imag-rectangular x y))
+  ((get 'make-from-real-imag 'rectangular) x y))
 
 (define (make-from-mag-ang r a)
-  (make-from-mag-ang-polar r a))
+  ((get 'make-from-mag-ang 'polar) r a))
+
+(define (put op types fn)
+  (display "Implementation pending"))
+
+(define (get op types)
+  (display "Implementation pending"))
