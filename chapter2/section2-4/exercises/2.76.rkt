79968109d3a1f50fb7a389e7b7bd88940f89dcec
ftambara <98750668+ftambara@users.noreply.github.com>
2023-09-30 16:44:21 -0300

    Add exercise 2.76.rkt

---
+++ chapter2/section2-4/exercises/2.76.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+
+;; Changes that must be made to add types and operations:
+
+;; Generic operations with explicit dispatch
+;;  For each new type, all generic operations that will be used on it
+;;  need to be modified to dispatch to the new operations.
+;;  For each new generic operation, all existing types that the
+;;  procedure is intended to be used with must be considered.
+
+;; Data-directed programming
+;;  Each new type must define its own versions of the generic
+;;  operations and register them.
+;;  New operations must be added to each type operation definition
+;;  and registration procedures.
+
+;; Message passing
+;;  New types result in a new constructor, with corresponding versions
+;;  of existing operations.
+;;  To add a new operation, all existing dispatches must be modified
+;;  to understand the new message.
+
+
+;; Most appropriate for:
+
+;; Adding types
+;;  Adding types is easy with either data-directed or message passing
+;;  styles, since new code is clumped together and existing code is
+;;  not affected.
+
+;; Adding operations
+;;  Paradoxically, the seemingly unconfortable method of delegating
+;;  dispatch responsibility to operations seems the most flexible for
+;;  adding new operations, since existing code suffers no
+;;  modifications.
