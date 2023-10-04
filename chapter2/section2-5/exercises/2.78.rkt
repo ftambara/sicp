92be6e0c4c907167c0be3048fe9ddb91168204ef
ftambara <98750668+ftambara@users.noreply.github.com>
2023-10-04 07:46:15 -0300

    Add tests to ex. 2.78

--- chapter2/section2-5/exercises/2.78.rkt
+++ chapter2/section2-5/exercises/2.78.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require rackunit)
+
 
 (define (literal? expr)
   (or (number? expr) (symbol? expr)))
@@ -19,3 +21,12 @@
   (cond ((pair? datum) (cdr datum))
         ((literal? datum) datum)
         (error "Bad tagged datum: CONTENTS" datum)))
+
+;; Tests
+(check-equal? (type-tag 10) 'scheme-number)
+(check-equal? (type-tag (list 'complex (cons 4 5)))
+              'complex)
+
+(check-equal? (contents 10) 10)
+(check-equal? (contents (list 'complex (cons 4 5)))
+              (list (cons 4 5)))
