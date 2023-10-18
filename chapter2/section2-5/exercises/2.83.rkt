221cfb419ae24f7cf667adf8d4366db4acb93b13
ftambara <98750668+ftambara@users.noreply.github.com>
2023-10-18 07:49:30 -0300

    Add exercise 2.83

---
+++ chapter2/section2-5/exercises/2.83.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(define get void)
+(define put void)
+
+;; From the book
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types: APPLY-GENERIC"
+                 (list op type-tags))))))
+
+;; Extend packages defined in this section's notes
+(define (install-integer-package)
+  (define (raise number)
+    ((get 'make 'rational) number 1))
+
+  (put 'raise 'integer raise))
+
+(define (install-rational-package)
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+
+  (define (raise number)
+    ((get 'make 'real) (/ (numer number) (denom number))))
+
+  (put 'raise 'integer raise))
+
+(define (install-real-package)
+  (define (raise number)
+    ((get 'make-from-real-imag 'rectangular) number 0))
+
+  (put 'raise 'real raise))
+
+(define (raise number)
+  (apply-generic 'raise number))
