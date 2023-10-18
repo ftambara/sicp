e892384b33a69e8e214e7e1ad1804566ab1ca33a
ftambara <98750668+ftambara@users.noreply.github.com>
2023-10-17 21:05:22 -0300

    Add exercise 2.82

---
+++ chapter2/section2-5/exercises/2.82.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+(require rackunit)
+
+
+;; From the book (section 2.4)
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+;; To be defined
+(define get-coercion void)
+(define get void)
+
+;; Solution
+(define (unique lst)
+  (define (iter accum rest)
+    (if (empty? rest)
+        accum
+        (if (member (car rest) accum)
+            (iter accum (cdr rest))
+            (iter (cons (car rest) accum) (cdr rest)))))
+  (iter '() lst))
+
+(check-equal? (unique '(1 2 3 2 4 3))
+              '(4 3 2 1))
+
+(define (apply-generic op . args)
+  (let* ((type-tags (map type-tag args))
+         (proc (find-coerced-proc op args)))
+    (if proc
+        (apply proc (map contents args))
+        (error "No method for these types" op type-tags))))
+
+;; Untested and complicated
+(define (find-coerced-proc op args)
+  (define (rec types-from)
+    (if (empty? types-from)
+        null
+        (let* ((types-to (map type-tag args))
+               (coercions (filter-map (lambda (coercion) coercion)
+                                      (lambda (type-to)
+                                        (get-coercion (car types-from)
+                                                      type-to))
+                                      types-to)))
+          (if (< (length coercions) (length args))
+              (rec (cdr types-from))
+              (let ((coerced-args
+                     (map (lambda (arg) ((get-coercion (car types-from)
+                                                       (type-tag arg))
+                                         arg)
+                            args))))
+                (let ((proc (get op (map type-tag coerced-args))))
+                  (if proc proc null)))))))
+
+  (rec (unique (map type-tag args))))
+
