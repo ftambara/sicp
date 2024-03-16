ce8d08274f0407d73f121f8e4e7f27d0297b440c
ftambara <98750668+ftambara@users.noreply.github.com>
2024-03-16 20:05:43 -0300

    Add exercise 3.3

---
+++ chapter3/section3.1/exercises/3.3.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require rackunit)
+
+(define (make-account balance password)
+  (define (protect-by-pass f)
+    (lambda (pass-attempt arg)
+      (if (not (eq? pass-attempt password))
+        (error "Incorrect password")
+        (f arg))))
+  (define (account action)
+    (cond ((eq? action 'withdraw)
+           (lambda (amount)
+             (if (< balance amount)
+               (error ("Not enough funds"))
+               (begin
+                 (set! balance (- balance amount))
+                 balance))))
+          ((eq? action 'deposit)
+           (lambda (amount)
+             (set! balance (+ balance amount))
+             balance))
+          (else
+            (error "Incorrect action" action))))
+  (protect-by-pass account))
+
+
+(let ((acc (make-account 100 'secret)))
+  (check-exn exn:fail? (lambda () ((acc 'wrong-pass 'withdraw) 30)))
+  (check-exn exn:fail? (lambda () (acc 'secret 'wrong-action) 30))
+  (check-eq? ((acc 'secret 'withdraw) 30) 70)
+  (check-eq? ((acc 'secret 'withdraw) 30) 40)
+  (check-eq? ((acc 'secret 'deposit) 10) 50))
