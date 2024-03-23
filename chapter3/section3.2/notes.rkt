b7a730d1a255be001af50d4ba0d2cf991efd1259
ftambara <98750668+ftambara@users.noreply.github.com>
2024-03-23 09:19:04 -0300

    Add notes for the rest of section 3.2

--- chapter3/section3.2/notes.rkt
+++ chapter3/section3.2/notes.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;; Section 3.2.1
+;; Section 3.2
 
 ;; # Constructing procedures
 ;;  A new process object is created. The lambda expression is evaluated
@@ -21,3 +21,22 @@
 ;; # What set! does
 ;;  set! expressions find the firt frame of the environment that contain
 ;;  the given variable name, and modify the saved value for the new one.
+
+;; Procedure objects are created in one way only: by evaluating a λ-expression.
+;; (define (f x) <expr>) is syntactic sugar for (define f (λ (x) <expr>))
+
+;; The rest of the section are applied evaluations of the above concepts on
+;; increasingly complex examples involving local state and definitions.
+
+;; I had some trouble understanding which of the stated rules spcified that
+;; evaluating code that consists of another λ-expression should create the
+;; new procedure object in the context of the new environment (the one that
+;; binds the arguments of the enclosing λ-expression arguments), and not in
+;; the context of the the enclosing λ-expression environment itself.
+;; As I understand it now, it's defined in the application rule, the body of
+;; the procedure is evaluated in the context of the new environment.
+
+;; The key was understanding that there must be a one-to-one relation of
+;; procedure objects to lambda expressions. Without having this rule, the
+;; more intricate exambles involving `let`s (i.e exercise 3.10) got quite
+;; confusing.
