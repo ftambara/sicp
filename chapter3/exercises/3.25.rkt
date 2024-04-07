44c5a6454a32a834fd88aa23dc295e612106a472
ftambara <98750668+ftambara@users.noreply.github.com>
2024-04-06 22:33:18 -0300

    Add exercise 3.25

---
+++ chapter3/exercises/3.25.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+(require rackunit)
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((eq? key (mcar (mcar records))) (mcar records))
+        (else (assoc key (mcdr records)))))
+
+(define (make-table)
+  (let ((table (mcons '*table* null)))
+    (define (lookup keys)
+      (define (lookup-iter subtable rest-keys)
+        (let ((next (assoc (car rest-keys) (mcdr subtable))))
+          (if next
+            (if (null? (cdr rest-keys))
+              (mcdr next)
+              (lookup-iter next (cdr rest-keys)))
+            false)))
+      (lookup-iter table keys))
+
+    (define (insert! keys value)
+      (define (create-subtables keys value tail)
+        (if (null? keys)
+          value
+          (mcons (mcons (car keys)
+                        (create-subtables (cdr keys) value tail))
+                 tail)))
+
+      (define (insert-iter subtable rest-keys)
+        (let ((next (assoc (car rest-keys) (mcdr subtable))))
+          (if next
+            (if (null? (cdr rest-keys))
+              (set-mcdr! next value)
+              (insert-iter next (cdr rest-keys)))
+            (set-mcdr! subtable
+                       (create-subtables rest-keys value (mcdr subtable))))))
+
+      (insert-iter table keys)
+      'ok)
+
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+            ((eq? m 'insert!) insert!)
+            (else (error "invalid method" m))))
+
+    dispatch))
+
+;; Tests
+(define t (make-table))
+(check-equal? ((t 'insert!) '(a b c) 1) 'ok)
+(check-equal? ((t 'lookup) '(a b c)) 1)
+
+(check-equal? ((t 'insert!) '(a b d) 2) 'ok)
+(check-equal? ((t 'lookup) '(a b c)) 1)
+(check-equal? ((t 'lookup) '(a b d)) 2)
+
+(check-equal? ((t 'insert!) '(a b x y z) 10) 'ok)
+(check-equal? ((t 'lookup) '(a b c)) 1)
+(check-equal? ((t 'lookup) '(a b d)) 2)
+(check-equal? ((t 'lookup) '(a b x y z)) 10)
+
+(check-not-equal? ((t 'lookup) '(a b x y)) false)
