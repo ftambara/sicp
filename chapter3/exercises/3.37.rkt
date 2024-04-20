c5679d6e5858deaa4a681e03861a4aef631138a7
ftambara <98750668+ftambara@users.noreply.github.com>
2024-04-19 21:19:08 -0300

    Add exercise 3.37

---
+++ chapter3/exercises/3.37.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+(require
+  "constraints.rkt"
+  rackunit)
+
+(define (celsius-fahrenheit-converter-arithm! x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+(define (c+ a b)
+  (let ((s (make-connector)))
+    (adder! a b s)
+    s))
+
+(define (c* a b)
+  (let ((p (make-connector)))
+    (multiplier! a b p)
+    p))
+
+(define (c/ num denom)
+  (let ((quotient (make-connector)))
+    (multiplier! quotient denom num)
+    quotient))
+
+(define (cv value)
+  (let ((var (make-connector)))
+    (constant! value var)
+    var))
+
+(define C (make-connector))
+(define F (celsius-fahrenheit-converter-arithm! C))
+
+(set-value! C 20 'me)
+(check-eq? (get-value C) 20)
+(check-eq? (get-value F) 68)
+
+(forget-value! C 'me)
+(set-value! F -139 'me)
+(check-eq? (get-value F) -139)
+(check-eq? (get-value C) -95)
