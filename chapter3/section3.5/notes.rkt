be98c78f846f666ce4b7209bc62e8472ff1cebe4
ftambara <98750668+ftambara@users.noreply.github.com>
2024-05-05 19:09:05 -0300

    Add section 3.5.5 notes

--- chapter3/section3.5/notes.rkt
+++ chapter3/section3.5/notes.rkt
@@ -257,3 +257,50 @@ low is 10008 and high is 1000000.
                    0.001)
             1000)
 ;; 2.716923932235896
+
+
+;; Modularity of Functional Programs
+#|
+Modularizing via state
+
+(define rand
+(let ((x random-init))
+(lambda ()
+(set! x (rand-update x))
+x)))
+|#
+
+;; No state needed, only delayed evaluation magic
+(define (random-numbers)
+  (stream-cons
+    (random 4294967087)
+    (random-numbers)))
+
+(define (map-successive-pairs f s)
+  (stream-cons
+    (f (stream-first s) (stream-first (stream-rest s)))
+    (map-successive-pairs f (stream-rest (stream-rest s)))))
+
+(define cesaro-stream
+  (map-successive-pairs
+    (lambda (r1 r2) (= (gcd r1 r2) 1))
+    (random-numbers)))
+
+(define (monte-carlo experiment-stream passed failed)
+  (define (next passed failed)
+    (stream-cons
+      (/ passed (+ passed failed))
+      (monte-carlo
+        (stream-rest experiment-stream) passed failed)))
+  (if (stream-first experiment-stream)
+    (next (+ passed 1) failed)
+    (next passed (+ failed 1))))
+
+(define pi
+  (stream-map
+    (lambda (p) (sqrt (/ 6 p)))
+    (monte-carlo cesaro-stream 0 0)))
+
+;; “We can model the world as a collection of separate, time-bound, interacting
+;; objects with state, or we can model the world as a single, timeless,
+;; stateless unity.”
